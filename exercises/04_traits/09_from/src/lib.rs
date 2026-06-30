40d52d4bb1b3bb778c718332e6d373060d2f333f
ConquerorFY <ryanrlfy33@gmail.com>
2026-06-30 23:40:32 +0800

    feat: in progress

--- exercises/04_traits/09_from/src/lib.rs
+++ exercises/04_traits/09_from/src/lib.rs
@@ -4,6 +4,16 @@ pub struct WrappingU32 {
     value: u32,
 }
 
+impl From<i32> for WrappingU32 {
+    fn from(value: i32) -> Self {
+        WrappingU32 {
+            // value: value.into() // alternative
+            value: value as u32, // using as might have issues if over the limit causing truncate (typically from large to small (eg. i64 --> i32))
+        }
+        // value.into()         // alternative #2
+    }
+}
+
 fn example() {
     let wrapping: WrappingU32 = 42.into();
     let wrapping = WrappingU32::from(42);
