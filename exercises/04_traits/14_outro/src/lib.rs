ef96ce4161fd52bfa7b5fd177d972b9c2aca57d5
ConquerorFY <ryanrlfy33@gmail.com>
2026-07-01 23:53:23 +0800

    feat: in progress

--- exercises/04_traits/14_outro/src/lib.rs
+++ exercises/04_traits/14_outro/src/lib.rs
@@ -8,3 +8,77 @@
 //   It should be possible to print its debug representation.
 //
 // Tests are located in the `tests` folder—pay attention to the visibility of your types and methods.
+use std::ops::Add;
+#[derive(Debug, Clone, Copy)]
+pub struct SaturatingU16 {
+    value: u16,
+}
+
+impl Add<Self> for SaturatingU16 {
+    type Output = Self;
+    fn add(self, other: Self) -> Self::Output {
+        SaturatingU16 {
+            value: self.value.saturating_add(other.value),
+        }
+    }
+}
+
+impl Add<&Self> for SaturatingU16 {
+    type Output = Self;
+    fn add(self, other: &Self) -> Self::Output {
+        SaturatingU16 {
+            value: self.value.saturating_add(other.value),
+        }
+    }
+}
+
+impl Add<u16> for SaturatingU16 {
+    type Output = Self;
+    fn add(self, other: u16) -> Self::Output {
+        SaturatingU16 {
+            value: self.value.saturating_add(other),
+        }
+    }
+}
+
+impl PartialEq<u16> for SaturatingU16 {
+    fn eq(&self, other: &u16) -> bool {
+        self.value == *other
+    }
+}
+
+impl PartialEq for SaturatingU16 {
+    fn eq(&self, other: &Self) -> bool {
+        self.value == other.value
+    }
+}
+
+impl From<u16> for SaturatingU16 {
+    fn from(value: u16) -> Self {
+        SaturatingU16 { value }
+    }
+}
+
+impl From<u8> for SaturatingU16 {
+    fn from(value: u8) -> Self {
+        SaturatingU16 {
+            value: value.into(),
+        }
+    }
+}
+
+impl From<&u16> for SaturatingU16 {
+    fn from(value: &u16) -> Self {
+        SaturatingU16 {
+            value: (*value).into(),
+        }
+    }
+}
+
+impl From<&u8> for SaturatingU16 {
+    fn from(value: &u8) -> Self {
+        SaturatingU16 {
+            value: (*value).into(),
+        }
+    }
+}
