9dccd42aba2a81342a7de52a6663dd0d9cf0dd00
ConquerorFY <ryanrlfy33@gmail.com>
2026-07-06 23:08:57 +0800

    feat: in progress

--- exercises/05_ticket_v2/07_unwrap/src/lib.rs
+++ exercises/05_ticket_v2/07_unwrap/src/lib.rs
@@ -2,7 +2,33 @@
 //   When the description is invalid, instead, it should use a default description:
 //   "Description not provided".
 fn easy_ticket(title: String, description: String, status: Status) -> Ticket {
-    todo!()
+    let ticket = Ticket::new(title.clone(), description, status.clone());
+    match ticket {
+        Ok(ticket) => ticket,
+        Err(err) => {
+            if (err == "Title cannot be empty" || err == "Title cannot be longer than 50 bytes") {
+                panic!("{}", err)
+            }
+            Ticket::new(
+                title.clone(),
+                String::from("Description not provided"),
+                status.clone(),
+            )
+            .unwrap()
+        }
+    }
+
+    // Alternative solution
+    // match Ticket::new(title.clone(), description, status.clone()) {
+    //     Ok(ticket) => ticket,
+    //     Err(error) => {
+    //         if error.contains("Description") {
+    //             Ticket::new(title, "Description not provided".to_string(), status).unwrap()
+    //         } else {
+    //             panic!("{error}");
+    //         }
+    //     }
+    // }
 }
 
 #[derive(Debug, PartialEq, Clone)]
